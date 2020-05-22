c556b1fdd1b66f437853cb3ba310b30be9fef308
csmithid <jeff.cole.smith@gmail.com>
2020-05-22 17:51:31 -0400

    Mark 1

---
+++ bot.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+var _ = require("lodash");
+const words = require("./words.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const schedule = require("node-schedule");
+var natural = require("natural");
+
+console.log(process.env.BOT_TOKEN);
+
+client.on("ready", async () => {
+  console.log("Sketchybot is ready to draw!");
+  console.log(client.channels);
+  try {
+    channel = await client.channels.fetch("713459935836635218");
+    //channel.send("Sketchy-bot is rebooted!");
+  } catch (err) {
+    console.log(err);
+  }
+  scheduleMessage();
+});
+
+const scheduleMessage = () => {
+  const dailyMessage = schedule.scheduleJob("0 9 * * *", async () => {
+    console.log("Posting a new challenge.");
+    try {
+      channel = await client.channels.fetch("713459935836635218");
+      channel.send(
+        `@here ✒ The daily sketch challenge: draw a ${getRandomItem()}!`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+client.on("message", (msg) => {
+  if (
+    natural.JaroWinklerDistance(msg.content, "Should I sketch every day?") >=
+    0.9
+  ) {
+    msg.reply("yes.");
+  }
+});
+
+client.on("message", (msg) => {
+  if (natural.JaroWinklerDistance(msg.content, "What should I draw?") >= 0.9) {
+    msg.reply(`try drawing a ${getRandomItem()}!`);
+  }
+});
+
+const getRandomItem = () => {
+  return words.nouns[_.floor(_.random(0, words.nouns.length - 1))];
+};
+
+client.login(process.env.BOT_TOKEN);
